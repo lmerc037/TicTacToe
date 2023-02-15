baf1ee38ddfad16c5d211702d17162d2ec2f1d3c
lmerc037 <lmerc037@uottawa.ca>
2023-02-14 21:00:37 -0500

    new restart and switch mode function

--- script.js
+++ script.js
@@ -7,6 +7,29 @@ let currentPlayer = "x";
 let user1Wins = 0;
 let user2Wins = 0;
 
+let mode = "user-vs-computer";
+let computerWins = 0;
+let userWins = 0;
+
+document.querySelectorAll("input[name='mode']").forEach(function (input) {
+  input.addEventListener("change", function () {
+    mode = input.value;
+    restart();
+
+    if (mode === "user-vs-computer") {
+      document.querySelector("#user-wins").style.display = "block";
+      document.querySelector("#computer-wins").style.display = "block";
+      document.querySelector("#user1-wins").style.display = "none";
+      document.querySelector("#user2-wins").style.display = "none";
+    } else {
+      document.querySelector("#user-wins").style.display = "none";
+      document.querySelector("#computer-wins").style.display = "none";
+      document.querySelector("#user1-wins").style.display = "block";
+      document.querySelector("#user2-wins").style.display = "block";
+    }
+  });
+});
+
 resetButton.addEventListener("click", function () {
   restart();
 });
@@ -15,25 +38,50 @@ for (let i = 0; i < cells.length; i++) {
   cells[i].addEventListener("click", function (event) {
     if (event.target.textContent === "" && currentPlayer === "x") {
       event.target.textContent = "x";
+      if (mode === "user-vs-computer") {
+        turn.textContent = "Turn: Computer";
+      } else {
+        turn.textContent = "Turn: User 2";
+      }
       if (checkForWin("x")) {
-        alert("Player X wins!");
+        if (mode === "user-vs-computer") {
+          result.textContent = "Player X wins!";
+          console.log(result.textContent);
+
+          userWins++;
+        } else {
+          result.textContent = "User 1 wins!";
+          user1Wins++;
+        }
         restart();
       } else if (checkForDraw()) {
-        alert("Draw!");
+        result.textContent = "Draw!";
         restart();
       } else {
         currentPlayer = "o";
-        if (mode === "user-vs-user") {
+        if (mode === "user-vs-computer") {
           computerTurn();
         }
       }
     } else if (event.target.textContent === "" && currentPlayer === "o") {
       event.target.textContent = "o";
+      if (mode === "user-vs-computer") {
+        turn.textContent = "Turn: User";
+      } else {
+        turn.textContent = "Turn: User 1";
+      }
       if (checkForWin("o")) {
-        alert("Player O wins!");
+        if (mode === "user-vs-computer") {
+          result.textContent = "Computer wins!";
+          console.log(result.textContent);
+          computerWins++;
+        } else {
+          result.textContent = "User 2 wins!";
+          user2Wins++;
+        }
         restart();
       } else if (checkForDraw()) {
-        alert("Draw!");
+        result.textContent = "Draw!";
         restart();
       } else {
         currentPlayer = "x";
@@ -66,6 +114,15 @@ function checkForWin(player) {
   return false;
 }
 
+function checkForDraw() {
+  for (let i = 0; i < cells.length; i++) {
+    if (cells[i].textContent === "") {
+      return false;
+    }
+  }
+  return true;
+}
+
 function computerTurn() {
   let move = Math.floor(Math.random() * 9);
   while (cells[move].textContent !== "") {
@@ -85,24 +142,32 @@ function computerTurn() {
   }
 }
 
-function checkForDraw() {
-  for (let i = 0; i < cells.length; i++) {
-    if (cells[i].textContent === "") {
-      return false;
-    }
-  }
-  return true;
-}
-
 function restart() {
   for (let i = 0; i < cells.length; i++) {
     cells[i].textContent = "";
   }
   result.textContent = "";
-
-  if (currentPlayer === "x") {
-    turn.textContent = "Turn: User 1";
+  if (mode === "user-vs-computer") {
+    if (currentPlayer === "x") {
+      turn.textContent = "Turn: User";
+    } else {
+      turn.textContent = "Turn: Computer";
+    }
+    document.querySelector("#user-wins").textContent = `User wins: ${userWins}`;
+    document.querySelector(
+      "#computer-wins"
+    ).textContent = `Computer wins: ${computerWins}`;
   } else {
-    turn.textContent = "Turn: User 2";
+    if (currentPlayer === "x") {
+      turn.textContent = "Turn: User 1";
+    } else {
+      turn.textContent = "Turn: User 2";
+    }
+    document.querySelector(
+      "#user1-wins"
+    ).textContent = `User 1 wins: ${user1Wins}`;
+    document.querySelector(
+      "#user2-wins"
+    ).textContent = `User 2 wins: ${user2Wins}`;
   }
 }
